62fa0116ace98e637c28e3ddb7865029feb45935
rezaab69 <rezaab1369@gmail.com>
2023-06-04 19:41:28 +0330

    Update bvpn.sh

--- bvpn.sh
+++ bvpn.sh
@@ -3,8 +3,9 @@ OS=`uname -m`;
 yum update -y
 yum install epel-release -y
 yum install htop tmux screen -y
-sudo wget -O /usr/bin/badvpn-udpgw "https://mytelegrambot.ir/dl/badvpn-udpgw64"
+sudo wget -O /usr/bin/badvpn-udpgw "https://github.com/rezaab69/badvpntest/raw/main/badvpn-udpgw64"
 sudo touch /etc/rc.local
 sudo echo "\nnohup watch -n 120 screen -dmS udpvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 50" >> /etc/rc.local
 sudo chmod +x /usr/bin/badvpn-udpgw
 sudo nohup watch -n 120 screen -dmS udpvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 50 &
+
