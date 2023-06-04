9e2db0489e7af1d3cb2d65f95971fddfb70f85c1
rezaab69 <rezaab1369@gmail.com>
2023-06-04 21:42:17 +0330

    Update bvpn.sh

--- bvpn.sh
+++ bvpn.sh
@@ -7,5 +7,6 @@ sudo wget -O /usr/bin/badvpn-udpgw "https://github.com/rezaab69/badvpntest/raw/m
 sudo touch /etc/rc.local
 sudo echo "\nnohup watch -n 120 screen -dmS udpvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 --max-connections-for-client 50" >> /etc/rc.local
 sudo chmod +x /usr/bin/badvpn-udpgw
+pkill badvpn-udpgw
 sudo nohup watch -n 120 screen -dmS udpvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 --max-connections-for-client 50 &
 
